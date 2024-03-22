73b5f9436268182e6a994b48ddd71dc19cf12b24
Risbel Suarez <risbel961019@gmail.com>
2024-03-21 20:48:35 -0400

    feat: Add action to fetch confirmations

--- src/app/actions/confirmActions.tsx
+++ src/app/actions/confirmActions.tsx
@@ -1,60 +1,51 @@
-"use server";
-
 import { revalidateTag } from "next/cache";
 import { IConfirmations } from "./confirmTypes";
 import { Person } from "../confirm/components/GuestsInput";
 
 export const addConfirmations = async (e: FormData, inputList: Person[]) => {
-  try {
-    const name = e.get("name")?.toString();
-    const phone = e.get("phone")?.toString();
-    const email = e.get("email")?.toString();
-    const address = e.get("address")?.toString();
-    const company = e.get("company")?.toString();
-    const guests: Person[] = inputList;
+  const name = e.get("name")?.toString();
+  const phone = e.get("phone")?.toString();
+  const email = e.get("email")?.toString();
+  const address = e.get("address")?.toString();
+  const company = e.get("company")?.toString();
+  const guests: Person[] = inputList;
 
-    if (!name || !phone || !email) {
-      return;
-    }
-
-    const newSubscription = {
-      name,
-      phone,
-      email,
-      address,
-      company,
-      guests,
-    };
-
-    await fetch("https://artworld-api.myaipeople.com/api/subscriptions/", {
-      method: "POST",
-      body: JSON.stringify(newSubscription),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    revalidateTag("subscriptions");
-  } catch (error) {
-    return {
-      error,
-    };
+  if (!name || !phone || !email) {
+    return;
   }
+
+  const newSubscription = {
+    name,
+    phone,
+    email,
+    address,
+    company,
+    guests,
+  };
+
+  await fetch("https://artworld-api.myaipeople.com/api/subscriptions/", {
+    method: "POST",
+    body: JSON.stringify(newSubscription),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  revalidateTag("subscriptions");
 };
 
 export const getConfirmations = async () => {
-  try {
-    const res = await fetch("https://artworld-api.myaipeople.com/api/subscriptions/", {
-      cache: "no-cache",
-      next: {
-        tags: ["subscriptions"],
-      },
-    });
-    const subscriptions: IConfirmations = await res.json();
-    return subscriptions;
-  } catch (error) {
-    return {
-      error,
-    };
+  const res = await fetch("https://artworld-api.myaipeople.com/api/subscriptions/", {
+    next: {
+      tags: ["subscriptions"],
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  const subscriptions: IConfirmations = await res.json();
+
+  return subscriptions;
 };
