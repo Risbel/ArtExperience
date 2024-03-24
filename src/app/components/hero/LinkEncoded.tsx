ef5f99e6ef4777db8eb75b0aeb9ae99a6833d07f
Risbel Suarez <risbel961019@gmail.com>
2024-03-24 09:19:52 -0400

    feat: Add `tooltip` in `LinkEncoded`

--- src/app/components/hero/LinkEncoded.tsx
+++ src/app/components/hero/LinkEncoded.tsx
@@ -3,6 +3,7 @@
 import { LockIcon, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 const LinkEncoded = () => {
   const router = useRouter();
@@ -23,12 +24,19 @@ const LinkEncoded = () => {
   }, [clickCount]);
 
   return (
-    <button
-      className="hover:-translate-y-[3px] hover:shadow-lg transition-transform duration-300"
-      onClick={redirectEncoded}
-    >
-      <LockIcon className="stroke-primary fill-white/80" />
-    </button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger
+          className="hover:-translate-y-[3px] hover:shadow-lg transition-transform duration-300"
+          onClick={redirectEncoded}
+        >
+          <LockIcon className="stroke-primary fill-white/80" />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>reserved for admins</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
